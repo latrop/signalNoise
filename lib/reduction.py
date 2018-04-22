98afa1cbdab19a5970b259a4fb33876ed52b4f87
latrop <savchenko.s.s@gmail.com>
2018-04-23 00:52:57 +0300

    Clobber replaced with overwrite

--- lib/reduction.py
+++ lib/reduction.py
@@ -22,7 +22,7 @@ def make_master_dark(pathToDir):  # TODO What if no dark files found?
         # we need to create a new one
         masterDarkData = np.median([safe_open_fits(fName)[0].data for fName in lastDarks], axis=0)
         masterDarkHDU = pyfits.PrimaryHDU(data=masterDarkData)
-        masterDarkHDU.writeto(outPath, clobber=True)
+        masterDarkHDU.writeto(outPath, overwrite=True)
     else:
         # else hust use existing master dark
         masterDarkData = safe_open_fits(outPath)[0].data.copy()
@@ -39,7 +39,7 @@ def make_master_bias(pathToDir):  # the same quesion
         allBiases = glob.glob(path.join(pathToDir, "bias*.FIT"))
         masterBiasData = np.median([safe_open_fits(fName)[0].data for fName in allBiases], axis=0)
         masterBiasHDU = pyfits.PrimaryHDU(data=masterBiasData)
-        masterBiasHDU.writeto(outPath, clobber=True)
+        masterBiasHDU.writeto(outPath, overwrite=True)
     else:
         masterBiasData = safe_open_fits(outPath)[0].data.copy()
     return masterBiasData
